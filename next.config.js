2e35fa39119e9b822efd4bae64b21498f3001410
wj77998 <wj77998@qq.com>
2019-10-11 10:29:48 +0800

    🐛fix:  bug fixed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withPlugins = require('next-compose-plugins')
+const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const path = require('path')
 const humps = require('humps')
 const webpack = require('webpack')
-const { name } = require('./package')
+const { name , version } = require('./package')
 const { resolve } = path
 const { config: dotConfig } = require('dotenv')
 const DotenvWebpack = require('dotenv-webpack')
@@ -12,118 +13,112 @@ const withLess = require('@zeit/next-less')
 const withCss = require('@zeit/next-css')
 
 dotConfig({
-  path: path.join(__dirname, 'env', 'common.env'),
+  path: path.join(__dirname, 'env', 'common.env')
 })
 
 dotConfig({
-  path: path.join(__dirname, 'env', `${NODE_ENV}.env`),
+  path: path.join(__dirname, 'env', `${NODE_ENV}.env`)
 })
+
+let PUBLIC_PATH = process.env.PUBLIC_PATH
+if (PUBLIC_PATH.indexOf('http') > -1) {
+  PUBLIC_PATH = `${PUBLIC_PATH}/${version}`
+}
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
   // tslint:disable-next-line:no-empty
-  require.extensions['.css'] = file => { }
+  require.extensions['.css'] = (file) => { }
   // tslint:disable-next-line:no-empty
-  require.extensions['.less'] = file => { }
+  require.extensions['.less'] = (file) => { }
 }
 
-module.exports = withPlugins([[withLess, {
-  lessLoaderOptions: {
-    javascriptEnabled: true
-  }
-}], withCss], {
-  useFileSystemPublicRoutes: false,
-  assetPrefix: process.env.PUBLIC_PATH,
-  webpack: (config, { isServer }) => {
-    config.output.publicPath = process.env.PUBLIC_PATH
-    const originalEntry = config.entry
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '~': resolve('src'),
-      '@': resolve('src/client'),
-      static: resolve('static/')
-    }
-    config.output.jsonpFunction = `__${humps.pascalize(name)}WebpackJsonp__`
-    config.resolve.extensions.push('.less')
-    config.entry = async () => {
-      const entries = await originalEntry()
-      if (
-        entries['main.js'] &&
-        !entries['main.js'].includes('./src/client/lib/polyfills.ts')
-      ) {
-        entries['main.js'].unshift('./src/client/lib/polyfills.ts')
-      }
-      return entries
-    }
-    config.plugins = config.plugins || []
-    config.plugins = [
-      ...config.plugins,
-      new DotenvWebpack({
-        path: path.join(__dirname, 'env', 'common.env'),
-        systemvars: true
-      }),
-      new DotenvWebpack({
-        path: path.join(__dirname, 'env', `${NODE_ENV}.env`),
-        systemvars: true
-      }),
-      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn|en/),
-    ]
-    config.module.rules = config.module.rules || []
-    config.module.rules = [
-      ...config.module.rules,
+module.exports = withPlugins(
+  [
+    withTypescript,
+    [
+      withLess,
       {
-        test: /\.(txt|jpg|png|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              context: '',
-              outputPath: 'static',
-              publicPath: '/_next/static',
-              name: 'assets/[name].[hash].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: [
-                resolve(process.cwd(), 'src/client/less/_mixin.less'),
-                resolve(process.cwd(), 'src/client/less/_color.less')
-              ]
-            }
-          }
-        ]
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+        }
       }
-    ]
-    if (isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin({
-        tsconfig: './tsconfig.json',
-      }))
-
-      const antStyles = /antd\/.*?\/style.*?/
-      const origExternals = [...config.externals]
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
-          } else {
-            callback()
-          }
-        },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+    ],
+    withCss
+  ],
+  {
+    useFileSystemPublicRoutes: false,
+    assetPrefix: PUBLIC_PATH,
+    webpack: (config, { isServer }) => {
+      config.output.publicPath = PUBLIC_PATH
+      const originalEntry = config.entry
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '~': resolve('src'),
+        '@': resolve('src/client'),
+        static: resolve('static/')
+      }
+      config.output.jsonpFunction = `__${humps.pascalize(name)}WebpackJsonp__`
+      config.resolve.extensions.push('.less')
+      config.entry = async () => {
+        const entries = await originalEntry()
+        if (
+          entries['main.js'] &&
+          !entries['main.js'].includes('./src/client/lib/polyfills.ts')
+        ) {
+          entries['main.js'].unshift('./src/client/lib/polyfills.ts')
+        }
+        return entries
+      }
+      config.plugins = config.plugins || []
+      config.plugins = [
+        ...config.plugins,
+        new DotenvWebpack({
+          path: path.join(__dirname, 'env', 'common.env'),
+          systemvars: true
+        }),
+        new DotenvWebpack({
+          path: path.join(__dirname, 'env', `${NODE_ENV}.env`),
+          systemvars: true
+        }),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn|en/)
       ]
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: 'null-loader',
-      })
+      config.module.rules = config.module.rules || []
+      config.module.rules = [
+        ...config.module.rules,
+        {
+          test: /\.(txt|jpg|png|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                context: '',
+                outputPath: 'static',
+                publicPath: `${(PUBLIC_PATH && PUBLIC_PATH !== '/') ? PUBLIC_PATH : '' }/_next/static`,
+                name: 'assets/[name].[hash].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: [
+                  resolve(process.cwd(), 'src/client/less/_mixin.less'),
+                  resolve(process.cwd(), 'src/client/less/_color.less')
+                ]
+              }
+            }
+          ]
+        }
+      ]
+      if (isServer) {
+        config.plugins.push(new ForkTsCheckerWebpackPlugin())
+      }
+      return config
     }
-    return config
   }
-})
+)
